c395dd38b9c4afda6e63f8d18cde200f24411686
BUKET POLAT <polatbuket@hotmail.com>
2021-01-23 01:27:38 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Listeleniyor.. "+musteri.Adi);
+        }
+
+        public void Ekle(string musteriAdi, string musteriSoyadi)
+        {
+            Console.WriteLine("Ekleniyor.. "+musteriAdi);
+        }
+
+        public void Sil(int musteriId)
+        {
+            Console.WriteLine("Siliniyor.. "+musteriId);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("napıcam hiçbi şe bilmiyom");
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.Id = 123;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Buket";
+            musteri2.Soyadi = "Polat";
+            musteri2.Id = 456;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ahmet";
+            musteri3.Soyadi = "Mehmet";
+            musteri3.Id = 789;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+            musteriManager.Ekle("Ali","A");
+            musteriManager.Ekle("Veli","B");
+
+            musteriManager.Sil(789);
+
+        }
+    }
+}
